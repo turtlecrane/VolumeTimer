2d0c0a0631c459463d80259c8af1f6f4ee389cc6
turtlecrane <suhan128@naver.com>
2024-03-07 01:04:34 +0900

    UI제작

---
+++ MuteTimer/Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public TMP_Text nowTimer_Text;
+
+    public TMP_InputField timerHours;
+    public TMP_InputField timerMinutes;
+
+    public Slider amountVolume;
+    
+    public TMP_Text nowVolumeAmount;
+    public TMP_Text settingTimer_Text;
+}
